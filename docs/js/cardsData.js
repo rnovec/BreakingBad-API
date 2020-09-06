9264a54602f537f515a43a5dac0b76bd3fd4c58a
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-09-05 20:16:55 -0500

    native fetch function

--- docs/js/cardsData.js
+++ docs/js/cardsData.js
@@ -73,166 +73,112 @@ let cardsData = [
     category: 'Breaking Bad',
     better_call_saul_appearance: []
   },
-   {
-        "char_id": 6,
-        "name": "Marie Schrader",
-        "birthday": "Unknown",
-        "occupation": [
-            "Housewife",
-            "Clepto"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/1/10/Season_2_-_Marie.jpg/revision/latest?cb=20120617211645",
-        "status": "Alive",
-        "nickname": "Marie",
-        "appearance": [
-            1,
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Betsy Brandt",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    },
-    {
-        "char_id": 7,
-        "name": "Mike Ehrmantraut",
-        "birthday": "Unknown",
-        "occupation": [
-            "Hitman",
-            "Private Investigator",
-            "Ex-Cop"
-        ],
-        "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_mike-ehrmantraut-lg.jpg",
-        "status": "Deceased",
-        "nickname": "Mike",
-        "appearance": [
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Jonathan Banks",
-        "category": "Breaking Bad, Better Call Saul",
-        "better_call_saul_appearance": [
-            1,
-            2,
-            3,
-            4
-        ]
-    },
-    {
-        "char_id": 8,
-        "name": "Saul Goodman",
-        "birthday": "Unknown",
-        "occupation": [
-            "Lawyer"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/1/16/Saul_Goodman.jpg/revision/latest?cb=20120704065846",
-        "status": "Alive",
-        "nickname": "Jimmy McGill",
-        "appearance": [
-            2,
-            3,
-            4,
-            5
-        ],
-        "portrayed": "Bob Odenkirk",
-        "category": "Breaking Bad, Better Call Saul",
-        "better_call_saul_appearance": [
-            1,
-            2,
-            3,
-            4
-        ]
-    },
-    {
-        "char_id": 9,
-        "name": "Gustavo Fring",
-        "birthday": "Unknown",
-        "occupation": [
-            "Los-Pollos co-Founder",
-            "Philanthropist",
-            "Cartel Leader"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/1/1f/BCS_S4_Gustavo_Fring.jpg/revision/latest?cb=20180824195925",
-        "status": "Deceased",
-        "nickname": "Gus",
-        "appearance": [
-            2,
-            3,
-            4
-        ],
-        "portrayed": "Giancarlo Esposito",
-        "category": "Breaking Bad, Better Call Saul",
-        "better_call_saul_appearance": [
-            3,
-            4
-        ]
-    },
-    {
-        "char_id": 10,
-        "name": "Hector Salamanca",
-        "birthday": "Unknown",
-        "occupation": [
-            "Former Senior Cartel Leader"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/b/b4/Hector_BCS.jpg/revision/latest?cb=20170810091606",
-        "status": "Deceased",
-        "nickname": "Don Hector",
-        "appearance": [
-            1,
-            2,
-            3,
-            4
-        ],
-        "portrayed": "Mark Margolis",
-        "category": "Breaking Bad, Better Call Saul",
-        "better_call_saul_appearance": [
-            2,
-            3,
-            4
-        ]
-    },
-    {
-        "char_id": 11,
-        "name": "Domingo Molina",
-        "birthday": "Unknown",
-        "occupation": [
-            "Meth Distributor"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/e/e7/Krazy8.png/revision/latest?cb=20130208041554",
-        "status": "Deceased",
-        "nickname": "Krazy-8",
-        "appearance": [
-            1
-        ],
-        "portrayed": "Maximino Arciniega",
-        "category": "Breaking Bad",
-        "better_call_saul_appearance": []
-    },
-    {
-        "char_id": 12,
-        "name": "Tuco Salamanca",
-        "birthday": "Unknown",
-        "occupation": [
-            "Meth Distributor"
-        ],
-        "img": "https://vignette.wikia.nocookie.net/breakingbad/images/a/a7/Tuco_BCS.jpg/revision/latest?cb=20170810082445",
-        "status": "Deceased",
-        "nickname": "Tuco",
-        "appearance": [
-            1,
-            2
-        ],
-        "portrayed": "Raymond Cruz",
-        "category": "Breaking Bad, Better Call Saul",
-        "better_call_saul_appearance": [
-            1,
-            2
-        ]
-    },
+  {
+    char_id: 6,
+    name: 'Marie Schrader',
+    birthday: 'Unknown',
+    occupation: ['Housewife', 'Clepto'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/1/10/Season_2_-_Marie.jpg/revision/latest?cb=20120617211645',
+    status: 'Alive',
+    nickname: 'Marie',
+    appearance: [1, 2, 3, 4, 5],
+    portrayed: 'Betsy Brandt',
+    category: 'Breaking Bad',
+    better_call_saul_appearance: []
+  },
+  {
+    char_id: 7,
+    name: 'Mike Ehrmantraut',
+    birthday: 'Unknown',
+    occupation: ['Hitman', 'Private Investigator', 'Ex-Cop'],
+    img:
+      'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_mike-ehrmantraut-lg.jpg',
+    status: 'Deceased',
+    nickname: 'Mike',
+    appearance: [2, 3, 4, 5],
+    portrayed: 'Jonathan Banks',
+    category: 'Breaking Bad, Better Call Saul',
+    better_call_saul_appearance: [1, 2, 3, 4]
+  },
+  {
+    char_id: 8,
+    name: 'Saul Goodman',
+    birthday: 'Unknown',
+    occupation: ['Lawyer'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/1/16/Saul_Goodman.jpg/revision/latest?cb=20120704065846',
+    status: 'Alive',
+    nickname: 'Jimmy McGill',
+    appearance: [2, 3, 4, 5],
+    portrayed: 'Bob Odenkirk',
+    category: 'Breaking Bad, Better Call Saul',
+    better_call_saul_appearance: [1, 2, 3, 4]
+  },
+  {
+    char_id: 9,
+    name: 'Gustavo Fring',
+    birthday: 'Unknown',
+    occupation: ['Los-Pollos co-Founder', 'Philanthropist', 'Cartel Leader'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/1/1f/BCS_S4_Gustavo_Fring.jpg/revision/latest?cb=20180824195925',
+    status: 'Deceased',
+    nickname: 'Gus',
+    appearance: [2, 3, 4],
+    portrayed: 'Giancarlo Esposito',
+    category: 'Breaking Bad, Better Call Saul',
+    better_call_saul_appearance: [3, 4]
+  },
+  {
+    char_id: 10,
+    name: 'Hector Salamanca',
+    birthday: 'Unknown',
+    occupation: ['Former Senior Cartel Leader'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/b/b4/Hector_BCS.jpg/revision/latest?cb=20170810091606',
+    status: 'Deceased',
+    nickname: 'Don Hector',
+    appearance: [1, 2, 3, 4],
+    portrayed: 'Mark Margolis',
+    category: 'Breaking Bad, Better Call Saul',
+    better_call_saul_appearance: [2, 3, 4]
+  },
+  {
+    char_id: 11,
+    name: 'Domingo Molina',
+    birthday: 'Unknown',
+    occupation: ['Meth Distributor'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/e/e7/Krazy8.png/revision/latest?cb=20130208041554',
+    status: 'Deceased',
+    nickname: 'Krazy-8',
+    appearance: [1],
+    portrayed: 'Maximino Arciniega',
+    category: 'Breaking Bad',
+    better_call_saul_appearance: []
+  },
+  {
+    char_id: 12,
+    name: 'Tuco Salamanca',
+    birthday: 'Unknown',
+    occupation: ['Meth Distributor'],
+    img:
+      'https://vignette.wikia.nocookie.net/breakingbad/images/a/a7/Tuco_BCS.jpg/revision/latest?cb=20170810082445',
+    status: 'Deceased',
+    nickname: 'Tuco',
+    appearance: [1, 2],
+    portrayed: 'Raymond Cruz',
+    category: 'Breaking Bad, Better Call Saul',
+    better_call_saul_appearance: [1, 2]
+  }
 ]
 
+fetch('https://www.breakingbadapi.com/api/characters?limit=10')
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (data) {
+        console.log(data)
+    });
 
+    
